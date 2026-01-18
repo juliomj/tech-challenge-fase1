3d0549608342c9e69d10790254482e6b374226ee
juliomj <datasci.jmj@gmail.com>
2026-01-18 17:32:34 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,14 @@ from datetime import timedelta
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "app.db")
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///" + os.path.join(BASE_DIR, "app.db")
+)
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-JWT_SECRET_KEY = "super-secret-key"
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
 JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=1)
 
